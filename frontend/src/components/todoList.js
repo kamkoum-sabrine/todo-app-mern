3a7c3af84e9481248d95af82b02ed21ce626eb4c
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-12-20 00:06:32 +0100

    [react] import Component

--- frontend/src/components/todoList.js
+++ frontend/src/components/todoList.js
@@ -1,7 +1,5 @@
-// import logo from '../assets/images/logo-xd-meta.png';
+import { Component } from 'react'
 
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faSignInAlt } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 
 
