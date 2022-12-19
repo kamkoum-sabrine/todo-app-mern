ee90e7eeff78419b1322bf1521b4680a6b4ad16c
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-12-19 22:35:55 +0100

    [node] change type of status field to Number

--- backend/src/models/todo.model.ts
+++ backend/src/models/todo.model.ts
@@ -4,8 +4,8 @@ import mongoosePaginate from "mongoose-paginate"
 let todoSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
-    priority: { type: Number, required: true },
-    status: { type: String, required: true },
+    priority: { type: Number, required: true }, //1 => HIGH, 2 => MEDIUM, 3 => LOW
+    status: { type: Number, required: true },//0 => INPROGRESS, 1 => DONE
 });
 
 todoSchema.plugin(mongoosePaginate)
