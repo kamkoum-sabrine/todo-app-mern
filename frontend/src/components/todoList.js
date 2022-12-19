1e7789e9ff39a9e36706821a33bcaf5636dc253c
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-12-20 00:05:47 +0100

    [react] change TodoList component to class

--- frontend/src/components/todoList.js
+++ frontend/src/components/todoList.js
@@ -5,32 +5,45 @@
 import React from 'react';
 
 
-export function TodoList() {
+export class TodoList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        }
 
 
-    return (
-        <div>
-            <div >
 
-                <div className="centered">
+    }
 
-                    <p className="firstLine"> T &nbsp; O &nbsp; D &nbsp; O &nbsp; . &nbsp; A &nbsp; P &nbsp; P </p>
+    render() {
+        return (
+            <div>
+                <div >
 
-                    <p className="secondLine">TODO Application</p>
+                    <div className="centered">
 
-                    <p>
-                        <input className="InputStyle" placeholder="create a new todo        
-                          &#xf0e0; " type="text" />
-                    </p >
-                    <button type="submit" className="btnStyle">Add</button>
+                        <p className="firstLine"> T &nbsp; O &nbsp; D &nbsp; O &nbsp; . &nbsp; A &nbsp; P &nbsp; P </p>
+
+                        <p className="secondLine">TODO Application</p>
+
+                        <p>
+                            <input className="InputStyle" placeholder="create a new todo        
+                              &#xf0e0; " type="text" />
+                        </p >
+                        <button type="submit" className="btnStyle">Add</button>
+
+
+                    </div >
+
+
 
 
                 </div >
-
-
-
-
             </div >
-        </div >
-    )
+        )
+    }
+
+
 }
