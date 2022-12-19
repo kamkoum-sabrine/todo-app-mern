d02cbeaf36fec86de6c032690465031f82541fe5
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-12-19 23:54:38 +0100

    [react] create todoList component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './assets/css/style.css'
+import { TodoList } from './components/todoList'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg text-center centered">
+
+      <TodoList />
     </div>
   );
 }
---
+++ frontend/src/components/todoList.js
@@ -0,0 +1,35 @@
+// import logo from '../assets/images/logo-xd-meta.png';
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faSignInAlt } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+
+
+export function TodoList() {
+
+
+    return (
+        <div>
+            <div >
+
+                <div className="centered">
+
+                    <p className="firstLine"> T &nbsp; O &nbsp; D &nbsp; O &nbsp; . &nbsp; A &nbsp; P &nbsp; P </p>
+
+                    <p className="secondLine">TODO Application</p>
+
+                    <p>
+                        <input className="InputStyle" placeholder="create a new todo        
+                          &#xf0e0; " type="text" />
+                        <button type="submit" className="InputStyle">Add</button>
+                    </p >
+
+                </div >
+
+
+
+
+            </div >
+        </div >
+    )
+}
