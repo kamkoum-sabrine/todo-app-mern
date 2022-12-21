e1a1f42d842a8a24f7ef7ef0f73b27f4c2686cb6
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-12-21 12:01:22 +0100

    [node] remove unused code

--- backend/src/controllers/todo.controller.ts
+++ backend/src/controllers/todo.controller.ts
@@ -4,17 +4,6 @@ import Todo from "../models/todo.model";
 
 
 export const findAll = (req: Request, res: Response) => {
-    const search = req.query.search || '';
-    const page: number = parseInt(req.query.page?.toString() || '1');
-    const size: number = parseInt(req.query.size?.toString() || '5');
-
-    // Todo.paginate({
-    //     message: { $regex: ".*(?i)" + search + ".*" },
-    //     subject: { $regex: ".*(?i)" + search + ".*" }
-    // }, { page: page, limit: size }, (err: any, role: any) => {
-    //     if (err) res.status(500).send(err);
-    //     else res.send(role);
-    // });
     Todo.find({}, (err: any, todo: any) => {
         if (err) res.status(500).send(err);
         else res.send(todo);
@@ -62,18 +51,16 @@ export const getUnfinishedTodo = (req: Request, res: Response) => {
     Todo.find({ status: 0 }, (err: any, todo: any) => {
         if (err) return res.status(500).send(err);
         else if (!todo) return res.status(404).send("Todo not found");
-        // else Todo.findById(req.params.id, (err: any, todo: any) => {
+
         return res.status(200).send(todo);
-        // });
+
     })
 }
 export const getfinishedTodo = (req: Request, res: Response) => {
     Todo.find({ status: 1 }, (err: any, todo: any) => {
         if (err) return res.status(500).send(err);
         else if (!todo) return res.status(404).send("Todo not found");
-        // else Todo.findById(req.params.id, (err: any, todo: any) => {
         return res.status(200).send(todo);
-        // });
     })
 }
 
